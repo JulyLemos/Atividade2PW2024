b1cde800009f09eed73c53693d66d4ccf73c8236
JulyLemos <julylemos1@hotmail.com>
2024-11-27 19:02:56 -0300

    teste

--- main.py
+++ main.py
@@ -19,3 +19,5 @@ app.mount("/static", StaticFiles(directory="static"))
 app.add_middleware(SessionMiddleware, secret_key="a4cc060da26e3251a72b73e241147adb7e050ac6b979911370744fb6ebbd16d46f420e480b83480f7242347f02688eda8712a7121c4611dc8743d17c607c7589")
 app.include_router(public_router)
 app.include_router(admin_router)
+
+#teste de commit
